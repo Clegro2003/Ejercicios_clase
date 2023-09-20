7e3f48ace20f1981011029b2c40760bf0b7d0b2e
Clegro2003 <126128686+Clegro2003@users.noreply.github.com>
2023-09-20 00:04:29 -0500

    Update RegistroCampeonato.java

--- Taller5/Ejercicio2-Taller5/src/ejercicio2/taller5/RegistroCampeonato.java
+++ Taller5/Ejercicio2-Taller5/src/ejercicio2/taller5/RegistroCampeonato.java
@@ -4,10 +4,64 @@
  */
 package ejercicio2.taller5;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author carlo
  */
 public class RegistroCampeonato {
-    
+
+    private String name;
+    private int timecompetition;
+    private double rewardWinner;
+    private ArrayList<Equipo> nequipos;
+
+    public RegistroCampeonato(String name, int timecompetition, double rewardWinner, ArrayList<Equipo> nequipos) {
+        this.name = name;
+        this.timecompetition = timecompetition;
+        this.rewardWinner = rewardWinner;
+        this.nequipos = nequipos;
+    }
+
+    public RegistroCampeonato(ArrayList<Equipo> nequipos) {
+        this.nequipos = new ArrayList();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTimecompetition() {
+        return timecompetition;
+    }
+
+    public void setTimecompetition(int timecompetition) {
+        this.timecompetition = timecompetition;
+    }
+
+    public double getRewardWinner() {
+        return rewardWinner;
+    }
+
+    public void setRewardWinner(double rewardWinner) {
+        this.rewardWinner = rewardWinner;
+    }
+
+    public ArrayList<Equipo> getNequipos() {
+        return nequipos;
+    }
+
+    public void setNequipos(ArrayList<Equipo> nequipos) {
+        this.nequipos = nequipos;
+    }
+
+    public void addEquipos(Equipo e) {
+        this.nequipos.add(e);
+    }
+
 }
